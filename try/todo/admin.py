8855ba7a286ab9bab0b229b2b8569fc7c9bb8bb1
Yashwant Soni <yashwantsoni009@gmail.com>
2018-03-10 16:37:23 +0530

    first commit with django

--- try/todo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import profile
-
-class profileAdmin(admin.ModelAdmin):
-	class Meta:
-		model = profile
-
-admin.site.register(profile,profileAdmin)
-# Register your models here.
